6a078013e1dbd81097dbfb23fb19e070a5f61253
BKPrograms <68819197+BKPrograms@users.noreply.github.com>
2020-07-31 11:29:07 +0400

    Added option to input a hashList text file and attack modes

--- hashCracker.py
+++ hashCracker.py
@@ -10,18 +10,20 @@ try:
 except ImportError:
     print("You seem to be missing some necessary libraries, please run pip install -r requirements.txt")
 
+
 def clearTerminal():
     if os.name == "posix":
         os.system("clear")
     elif os.name == "nts":
         os.system("cls")
 
+
 def getOptions():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-w", "--wordlist", dest="wordlistToUse",
                         help="The path to the wordlist to use during the attack")
-    parser.add_argument("-c", "--hash", dest="hashToCrack", help="The hash to crack")
+    parser.add_argument("-c", "--hash", dest="hashToCrack", help="The hash or .txt file of list of hashes to crack")
     parser.add_argument("-a", "--algorithm", dest="algorithmToUse",
                         help=f"The hashing algorithm to use, including: {hashlib.algorithms_guaranteed}")
 
@@ -30,7 +32,7 @@ def getOptions():
     if not options.wordlistToUse:
         parser.error("Specify a wordlist please, run python3 hashCracker.py --help for more info")
     elif not options.hashToCrack:
-        parser.error("Specify a hash please, run python3 hashCracker.py --help for more info")
+        parser.error("Specify a hash or list of hashes please, run python3 hashCracker.py --help for more info")
     elif not options.algorithmToUse:
         parser.error("Specify a hashing algorithm to use, run python3 hashCracker.py --help for more info")
     elif ".txt" not in options.wordlistToUse:
@@ -41,7 +43,7 @@ def getOptions():
     return options
 
 
-def crack(wordList, hash, algorithm):
+def singleHashcrack(wordList, hash, algorithm):
     found = False
     try:
         with open(wordList, mode="r") as wordlistFile:
@@ -57,10 +59,65 @@ def crack(wordList, hash, algorithm):
                     print(f"[{colored('-', 'red')}] {word.strip()} did not match! Moving onto next guess....")
 
             if found == False:
-                print("\nSorry, hash couldn't be found! Please try another wordlist or select a different hashing algorithm\n")
+                print(
+                    "\nSorry, hash couldn't be found! Please try another wordlist or select a different hashing algorithm\n")
 
     except FileNotFoundError:
         print("ERROR FILE NOT FOUND! Did you put the correct wordlist file path?")
+    except KeyboardInterrupt:
+        print("\nQuitting Program...Have a nice day!")
+
+
+def hashListCrack(wordList, hashList, algorithm):
+    try:
+        print(f"\nSelect {colored('ATTACK MODE', 'red')}")
+        print("1. Parallel attack, every hash gets compared to the word in the wordlist at it's corresponding position")
+        print("2. Intense attack, each hash is compared to EVERY word in the wordlist in order (will take longest time)")
+        print("0. Exit Hash-Cracker")
+        attackMode = input(f"{colored('Hash-Cracker > ', 'green')}")
+        if attackMode == "0":
+            print("Have a nice day!")
+            time.sleep(2)
+            clearTerminal()
+            exit()
+
+        while attackMode not in ["1", "2"]:
+            print("INVALID CHOICE!\n")
+            attackMode = input(f"{colored('Hash-Cracker > ', 'green')}")
+
+        with open(wordList, mode="r") as wordListFile:
+            listOfWords = list(wordListFile.readlines())
+            with open(hashList, mode="r") as hashListFile:
+                listOfHashes = list(hashListFile.readlines())
+                resultsString = ""
+                hashListFile.seek(0, 0)
+                if attackMode == "1":
+                    for i, hash in enumerate(hashListFile.readlines()):
+                        h = hashlib.new(algorithm)
+                        h.update(listOfWords[i].strip().encode('utf-8'))
+                        if hash.strip() == h.hexdigest():
+                            resultsString = resultsString + f"{hash.strip()} was cracked to be {colored(listOfWords[i], 'green')}\n"
+                            listOfHashes.remove(hash)
+
+                elif attackMode == "2":
+                    for hash in hashListFile.readlines():
+                        for word in listOfWords:
+                            h = hashlib.new(algorithm)
+                            h.update(word.strip().encode('utf-8'))
+                            if hash.strip() == h.hexdigest():
+                                resultsString = resultsString + f"{hash.strip()} was cracked to be {colored(word, 'green')}\n"
+                                listOfHashes.remove(hash)
+
+                print("\n" + resultsString)
+                if len(listOfHashes) > 0:
+                    for uncrackedHash in listOfHashes:
+                        print(
+                            f"{uncrackedHash.strip()} could not be cracked! Try a different wordlist? Or a different algorithm")
+    except FileNotFoundError:
+        print("ERROR FILE NOT FOUND! Did you put the correct wordlist file path AND the correct hashlist path?")
+    except KeyboardInterrupt:
+        print("\nQuitting Program...Have a nice day!")
+
 
 clearTerminal()
 
@@ -78,4 +135,9 @@ print(createdBy + profile)
 
 time.sleep(5)
 
-crack(wordList=optionsToUse.wordlistToUse, hash=optionsToUse.hashToCrack, algorithm=optionsToUse.algorithmToUse)
+if ".txt" in optionsToUse.hashToCrack:
+    hashListCrack(wordList=optionsToUse.wordlistToUse, hashList=optionsToUse.hashToCrack,
+                  algorithm=optionsToUse.algorithmToUse)
+else:
+    singleHashcrack(wordList=optionsToUse.wordlistToUse, hash=optionsToUse.hashToCrack,
+                    algorithm=optionsToUse.algorithmToUse)
